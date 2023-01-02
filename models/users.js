ce9b58223fc0aba1447c0d07fa009e475d5d8a04
test <medo0o0frh@gmail.com>
2023-01-02 20:34:34 +0100

    Router personfind

--- models/users.js
+++ models/users.js
@@ -1,14 +1,6 @@
 const mongoose =require('mongoose');
 const Schema = mongoose.Schema;
 
-/*var login = Schema({
-        _id:Schema.Types.ObjectId,
-        username:String,
-        password:String,
-        token:String
-});
-*/
-
 var users = Schema ({
         username: {
                 type: String,
